f22ecf566551307f0c82d7b343b36082d82da199
Sadie Burns <sadiejklol@gmail.com>
2024-10-05 06:51:02 -0400

    stupid photo makes me mald

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,16 +14,54 @@ export default async function Home() {
   return (
     <HydrateClient>
       <main>
+        <div className="game-wrap absolute object-none h-fit w-full z-10 bg-gradient-to-r from-cyan-500 to-blue-500 z-[-999]">
+          <img className="game-img absolute w-[100%] h-[220px] opacity-[0.5] object-none z--20 "
+          src="https://media.discordapp.net/attachments/1290909039017857046/1292050593258536960/sea-of-thieves-4.png?ex=67025362&is=670101e2&hm=1b85e6fc2030fc0d7bd0ac0e932062e62724ee58e2a02289a39223353aed55e4&=&format=webp&quality=lossless&width=2068&height=1164">
+          </img>
+        </div>
         <div className="grid grid-cols-[480px_minmax(0px,_0fr)_900px]">
           <div className="grid grid-rows-3 pl-[100px] pt-[145px]">
             <div className="update_progress text-white font-[600] text-[24px] leading-[28.8px] text-nowrap pl-[8px] pt-[8px]">
                 Update Your Progress!
+                <div className="games grid grid-cols-3">
+                  <img className="w-[75px] h-[102px] rounded-lg"
+                  src="https://media.discordapp.net/attachments/1290909039017857046/1292050591559848016/Warhammer_402C000_Space_Marine_2_cover_art.png?ex=67025362&is=670101e2&hm=00e52aeb5b72d6b230cdfc05a6b3bc75fbe381aa2da9a1edf4e3f596dca270e6&=&format=webp&quality=lossless&width=546&height=730">
+                  </img>
+                  <img className="w-[75px] h-[102px] rounded-lg"
+                  src="https://media.discordapp.net/attachments/1290909039017857046/1292050591559848016/Warhammer_402C000_Space_Marine_2_cover_art.png?ex=67025362&is=670101e2&hm=00e52aeb5b72d6b230cdfc05a6b3bc75fbe381aa2da9a1edf4e3f596dca270e6&=&format=webp&quality=lossless&width=546&height=730">
+                  </img>
+                  <img className="w-[75px] h-[102px] rounded-lg"
+                  src="https://media.discordapp.net/attachments/1290909039017857046/1292050591559848016/Warhammer_402C000_Space_Marine_2_cover_art.png?ex=67025362&is=670101e2&hm=00e52aeb5b72d6b230cdfc05a6b3bc75fbe381aa2da9a1edf4e3f596dca270e6&=&format=webp&quality=lossless&width=546&height=730">
+                  </img>
+                </div>
             </div>
-            <div className="friend_activity text-white font-[600] text-[24px] leading-[28.8px] pl-[48px] pt-[8px]">
-                Friend Activity
+            <div className="friend_activity text-white font-[600] text-[24px] leading-[28.8px] pl-[8px] pt-[7px]">
+                <p className="pl-[48px] pt-[0px]">Friend Activity</p>
+                <div className="games grid grid-cols-3">
+                  <img className="w-[75px] h-[102px] rounded-lg"
+                  src="https://media.discordapp.net/attachments/1290909039017857046/1292050591559848016/Warhammer_402C000_Space_Marine_2_cover_art.png?ex=67025362&is=670101e2&hm=00e52aeb5b72d6b230cdfc05a6b3bc75fbe381aa2da9a1edf4e3f596dca270e6&=&format=webp&quality=lossless&width=546&height=730">
+                  </img>
+                  <img className="w-[75px] h-[102px] rounded-lg"
+                  src="https://media.discordapp.net/attachments/1290909039017857046/1292050591559848016/Warhammer_402C000_Space_Marine_2_cover_art.png?ex=67025362&is=670101e2&hm=00e52aeb5b72d6b230cdfc05a6b3bc75fbe381aa2da9a1edf4e3f596dca270e6&=&format=webp&quality=lossless&width=546&height=730">
+                  </img>
+                  <img className="w-[75px] h-[102px] rounded-lg"
+                  src="https://media.discordapp.net/attachments/1290909039017857046/1292050591559848016/Warhammer_402C000_Space_Marine_2_cover_art.png?ex=67025362&is=670101e2&hm=00e52aeb5b72d6b230cdfc05a6b3bc75fbe381aa2da9a1edf4e3f596dca270e6&=&format=webp&quality=lossless&width=546&height=730">
+                  </img>
+                </div>
             </div>
-            <div className="new_releases text-white font-[600] text-[24px] leading-[28.8px] pl-[48px] pt-[8px]">
-              New Releases
+            <div className="new_releases text-white font-[600] text-[24px] leading-[28.8px] pl-[8px] pt-[7px]">
+             <p className="pl-[48px] pt-[0px]">New Releases</p>
+              <div className="games grid grid-cols-3">
+                  <img className="w-[75px] h-[102px] rounded-lg"
+                  src="https://media.discordapp.net/attachments/1290909039017857046/1292050591559848016/Warhammer_402C000_Space_Marine_2_cover_art.png?ex=67025362&is=670101e2&hm=00e52aeb5b72d6b230cdfc05a6b3bc75fbe381aa2da9a1edf4e3f596dca270e6&=&format=webp&quality=lossless&width=546&height=730">
+                  </img>
+                  <img className="w-[75px] h-[102px] rounded-lg"
+                  src="https://media.discordapp.net/attachments/1290909039017857046/1292050591559848016/Warhammer_402C000_Space_Marine_2_cover_art.png?ex=67025362&is=670101e2&hm=00e52aeb5b72d6b230cdfc05a6b3bc75fbe381aa2da9a1edf4e3f596dca270e6&=&format=webp&quality=lossless&width=546&height=730">
+                  </img>
+                  <img className="w-[75px] h-[102px] rounded-lg"
+                  src="https://media.discordapp.net/attachments/1290909039017857046/1292050591559848016/Warhammer_402C000_Space_Marine_2_cover_art.png?ex=67025362&is=670101e2&hm=00e52aeb5b72d6b230cdfc05a6b3bc75fbe381aa2da9a1edf4e3f596dca270e6&=&format=webp&quality=lossless&width=546&height=730">
+                  </img>
+                </div>
             </div>
           </div>
           <div className="pt-[85px]">
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,13 +7,13 @@ import {
 import { Input } from "./ui/input"
 
 export function SearchBar() {
-    return <Input type="search" placeholder="Search 40,000+ Games..." className="leading-[24px] text-[20px] border-none bg-[#07070721]"/>
+    return <Input type="search" placeholder="Search 40,000+ Games..." className="leading-[24px] text-[20px] border-none bg-[#0707077a]"/>
   }
            
 
 const Navbar = () => {
     return (
-        <Menubar className="flex justify-between bg-[#0707074F] h-[60px]">
+        <Menubar className="flex justify-between bg-[#0707074F] h-[60px] absolute w-[100%]">
             <MenubarMenu>
                 <MenubarTrigger className="leading-[38.4px] text-[32px] w-[164px] h-[38px] text-white pt-2">
                     ProjectGG
