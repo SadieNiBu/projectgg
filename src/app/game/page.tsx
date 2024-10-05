5179aebf4261624fb323d25d3c52ab8d3509231c
Isaiah <isaiahmstockton@gmail.com>
2024-10-05 05:13:09 -0400

    fix: incorrect import

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { LatestPost } from "~/app/_components/post";
+import { LatestPost } from "~/app/components/post";
 import User from "~/lib/models/User";
 import { getServerAuthSession } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
