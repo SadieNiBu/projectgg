0d907f0c5f33042cfac1b3da5a5a0e413e7e32b4
Sadie Burns <sadiejklol@gmail.com>
2024-10-06 04:40:47 -0400

    i cant look at his face anymore

--- src/components/personal_profile.tsx
+++ src/components/personal_profile.tsx
@@ -1,4 +1,6 @@
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+import Showcase from "./showcase";
+import Showcase_Item from "./showcase_item";
 import "~/styles/profile.css";
 
 export function ProfilePic() {
@@ -16,9 +18,7 @@ const PersonalProfile = () => {
             <ProfilePic />
             <p className="absolute font-[400] text-white text-[32px] leading-[38.4px] ml-[230px] mt-[90px]">User's Name</p>
             <p className="absolute font-[400] text-white text-[20px] leading-[24px] ml-[232px] bottom-[425px]">Hi everyone! My name is User's Name!<br />I really like to play video games, and this is my description of<br/> how many video games I love to play!</p>
-            <div className="rectangle ml-[180px] mt-[200px]">
-
-            </div>
+            <Showcase />
         </div>
     )
 }
---
+++ src/components/showcase_item.tsx
@@ -0,0 +1,11 @@
+import '~/styles/profile.css'
+
+const Showcase_Item = () => {
+    return (
+        <div className='showcase_item'>
+            <img className='rounded-xl' src="https://media.discordapp.net/attachments/1290909039017857046/1292050591559848016/Warhammer_402C000_Space_Marine_2_cover_art.png?ex=6702fc22&is=6701aaa2&hm=f6add47b001db5932f65dc0e8888672f598c2c7e670d12ae00dd2cc234cbf984&=&format=webp&quality=lossless&width=546&height=730"/>
+        </div>
+    )
+}
+
+export default Showcase_Item
