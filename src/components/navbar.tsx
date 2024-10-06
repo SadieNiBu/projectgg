7637e91daaba0e5caa2afd5887fb49a0fc6432be
Sadie Burns <sadiejklol@gmail.com>
2024-10-05 22:09:37 -0400

    updated discord w/ navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -23,8 +23,13 @@ const Navbar = () => {
                 <MenubarTrigger className="w-[820px] h-[41px] rounded-lg pt-2">
                     <SearchBar />
                 </MenubarTrigger>
-                <MenubarTrigger className="leading-[38.4px] text-[32px] w-[100px] h-[38px] text-white pt-2">
-                    Login
+                <MenubarTrigger className="leading-[38.4px] text-[32px] text-white">
+                    <a>
+                    <div className="grid grid-cols-2">
+                        <p>Login</p>
+                        <img className="w-[69px] justify-self-center -m-3" src="https://media.discordapp.net/attachments/1290909039017857046/1292301508955865098/discord-logo-discord-icon-transparent-free-png.png?ex=67033d11&is=6701eb91&hm=965e2b7f7c0dc41fd805f7aebf8e0e2ce66eb5cfb2e1e51f7728414ec6005ca6&=&format=webp&quality=lossless&width=1164&height=1164"/>
+                    </div>
+                    </a>
                 </MenubarTrigger>
             </MenubarMenu>
         </Menubar>
