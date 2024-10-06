6a03c3ea3ae9f2b1db80ddbef6efe1f1f6618cb0
Maximus Smith <maximusmith529@gmail.com>
2024-10-06 08:19:46 -0400

    w

--- src/app/search/[search]/page.tsx
+++ src/app/search/[search]/page.tsx
@@ -1,3 +1,5 @@
+import { api } from "~/trpc/server";
+
 interface SearchProps {
     params: {
       search: string;
@@ -6,7 +8,18 @@ interface SearchProps {
 
 export default async function Page({ params }: SearchProps)
 {
-    
+
+    // use search to get query of games
+    const search = api.igdb.searchGames({query: params.search, limit: 10});
+
+
+    // return a page with the search results which are just clickable names of games
+
+    return (
+        <div>
+            {(await search).map(game => <div>{game.name}</div>)}
+        </div>
+    );
 
 }
 
